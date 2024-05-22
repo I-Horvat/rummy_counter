190e16dbace9a4ba5e2ccb3736a417eef357391d
I-Horvat <vrhorvat1@gmail.com>
2024-05-23 00:53:35 +0200

    backend

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+from PIL import Image
+import io
+
+from testing.test_image import  process_image
+
+app = Flask(__name__)
+
+
+@app.route('/upload', methods=['POST'])
+def upload_image():
+    if 'image' not in request.files:
+        return jsonify({"error": "No image provided"}), 400
+
+    file = request.files['image']
+    if file.filename == '':
+        return jsonify({"error": "No image selected"}), 400
+
+    try:
+        image = Image.open(file.stream).convert("RGB")
+        points=process_image(image)
+        return jsonify({"points": f"{points}"}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- testing/test_image.py
+++ testing/test_image.py
@@ -3,11 +3,12 @@ import torch
 import torchvision
 from PIL import Image, ImageDraw, ImageFont
 from torchvision import tv_tensors
-from torchvision.models.detection.faster_rcnn import FastRCNNPredictor, FasterRCNN_ResNet50_FPN_Weights
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor, FasterRCNN_ResNet50_FPN_Weights, \
+    fasterrcnn_resnet50_fpn
 from torchvision.transforms import v2
-
-from testing.plotting import plot_sample
-from utils.util import symbols
+from torchvision.transforms import functional as F
+from testing.plotting import plot_sample,symbols
+from utils.util import symbol_to_point
 
 
 def draw_prediction(image, prediction, confidence_threshold=0.3):
@@ -32,36 +33,74 @@ def draw_prediction(image, prediction, confidence_threshold=0.3):
     return image
 
 
-if __name__ == '__main__':
+def test_image(image_path):
+    global in_features, model
     num_classes = 54
-
-    #dict_path = '../checkpoint_epoch_30.pth'
+    # dict_path = '../checkpoint_epoch_30.pth'
     model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights=FasterRCNN_ResNet50_FPN_Weights.DEFAULT)
     in_features = model.roi_heads.box_predictor.cls_score.in_features
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
     num_of_pixels = 1024
     model.eval()
-    image_path = '../WhatsApp Image 2024-05-10 at 11.39.36.jpeg'
-    #image_path = '../../../total/0a246d3b-afb8-4c23-9dc3-2038ecb4a2b2/original_image.png'
+    # image_path = '../../../total/0a246d3b-afb8-4c23-9dc3-2038ecb4a2b2/original_image.png'
     image = tv_tensors.Image(PIL.Image.open(image_path).convert("RGB"))
     image = image.float() / 255.0
-
     transform = v2.Compose([
         v2.ToImage(),
-        v2.ToDtype(torch.float32,scale=True),
+        v2.ToDtype(torch.float32, scale=True),
         v2.Resize((num_of_pixels, num_of_pixels), antialias=True),
-        #v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        # v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
-    image=transform(image)
-
+    image = transform(image)
     checkpoint_full_path = '../models/checkpoint_epoch_30.pth'
-    checkpoint=torch.load(checkpoint_full_path,map_location=torch.device('cpu'))
+    checkpoint = torch.load(checkpoint_full_path, map_location=torch.device('cpu'))
     model.load_state_dict(checkpoint['state_dict'])
-
     input_tensor = image.unsqueeze(0)
     with torch.no_grad():
         prediction = model(input_tensor)
     print(prediction)
-
-    #draw_prediction(image, prediction[0], confidence_threshold=0.3).show()
+    # draw_prediction(image, prediction[0], confidence_threshold=0.3).show()
     plot_sample(image, prediction, confidence_threshold=0.75)
+
+def process_image(image):
+
+    global in_features, model
+    num_classes = 54
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights=FasterRCNN_ResNet50_FPN_Weights.DEFAULT)
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+    num_of_pixels = 1024
+    model.eval()
+    image = tv_tensors.Image(image)
+    image = image.float() / 255.0
+    transform = v2.Compose([
+        v2.ToImage(),
+        v2.ToDtype(torch.float32, scale=True),
+        v2.Resize((num_of_pixels, num_of_pixels), antialias=True),
+        # v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+    image = transform(image)
+    checkpoint_full_path = 'models/checkpoint_epoch_30.pth'
+    checkpoint = torch.load(checkpoint_full_path, map_location=torch.device('cpu'))
+    model.load_state_dict(checkpoint['state_dict'])
+    input_tensor = image.unsqueeze(0)
+    with torch.no_grad():
+        prediction = model(input_tensor)
+
+    print(prediction)
+    points=predictions_to_points(prediction, threshold=0.75)
+    # draw_prediction(image, prediction[0], confidence_threshold=0.3).show()
+    plot_sample(image, prediction, confidence_threshold=0.75)
+    #get labels of card over threshold, summ the points recieved
+    return points
+
+def predictions_to_points(predictions, threshold=0.75):
+    points = 0
+    for i in range(len(predictions[0]['boxes'])):
+        if predictions[0]['scores'][i] > threshold:
+            points += symbol_to_point[symbols[predictions[0]['labels'][i].item() - 1]]
+    return points
+
+if __name__ == '__main__':
+    path= '../images/WhatsApp Image 2024-05-10 at 11.39.36.jpeg'
+    test_image(path)
